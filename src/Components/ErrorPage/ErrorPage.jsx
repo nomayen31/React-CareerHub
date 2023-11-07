618f865d3479c714f08baf1f87dd242c9cdda191
nomayen <nomayen.ohin@gmail.com>
2023-11-08 00:50:30 +0600

    nav bar

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Opps!</h2>
+            <button to='/'>Go to back home</button>
+        </div>
+    );
+};
+
+export default ErrorPage;
