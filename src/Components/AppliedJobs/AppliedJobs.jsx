4ff308f5c3013f6ffe0a2d3b7afb258ca7e02bbc
nomayen <nomayen.ohin@gmail.com>
2023-11-09 17:54:30 +0600

    filter jobs  jobs

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,22 @@ import AppliedJobsDetails from "../AppliedJobsDetails/AppliedJobsDetails";
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [appliedJobs, setAppliedJobs] = useState([])
+    const [displayJobs, setDisplayJobs] = useState([])
+
+
+    const handleJobsFIlter = filter =>{
+        if (filter === 'all') {
+            setDisplayJobs(appliedJobs)
+        }
+        else if(filter === 'remote'){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote')
+            setDisplayJobs(remoteJobs)
+        }else if(filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite')
+            setDisplayJobs(onsiteJobs)
+        }
+
+    }
     useEffect(() => {
         const storedJobIds = getstoredJobApplication();
         if (jobs.length > 0) {
@@ -19,23 +35,24 @@ const AppliedJobs = () => {
                 }
             }
             setAppliedJobs(JobsApplied)
+            setDisplayJobs(JobsApplied)
             console.log(jobs, storedJobIds, JobsApplied);
 
         }
-    }, [])
+    }, [jobs])
     return (
         <div className="mt-8">
             <details className="dropdown">
                 <summary className="m-1 btn">open or close</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                    <li><a>All</a></li>
-                    <li><a>Remote</a></li>
-                    <li><a>OnSite</a></li>
+                    <li onClick={()=> handleJobsFIlter('all')}><a>All</a></li>
+                    <li onClick={()=> handleJobsFIlter('remote')}><a>Remote</a></li>
+                    <li onClick={()=> handleJobsFIlter('onsite')}><a>OnSite</a></li>
 
                 </ul>
             </details>
             {
-                appliedJobs.map(appliedJob => <AppliedJobsDetails
+                displayJobs.map(appliedJob => <AppliedJobsDetails
                     appliedJob={appliedJob}
                     key={appliedJob.id}
                 ></AppliedJobsDetails>)
